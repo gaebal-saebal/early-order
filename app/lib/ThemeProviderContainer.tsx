ac8c336aadb4c0b1f3a42fc291ee4567acc799c7
rosenfence <rosenfence@gmail.com>
2023-07-18 22:37:41 +0900

    ✨Feat : OR-22

---
+++ app/lib/ThemeProviderContainer.tsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+const ThemeProviderContainer = ({ children, theme }: { children: React.ReactNode; theme: {} }) => {
+  return (
+    <>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </>
+  );
+};
+
+export default ThemeProviderContainer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { StyledP, StyledSection } from './MainStyle';
+
 export default function Home() {
-  return <main>메인</main>;
+  return (
+    <main>
+      <StyledSection>
+        <StyledP>ThemeProvider 테스트 중입니다</StyledP>
+      </StyledSection>
+    </main>
+  );
 }
