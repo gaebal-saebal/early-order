fc4195fdd8c4505d8e30995e82b4bb627b71f21a
js-ha <jshaha0911@gmail.com>
2023-07-22 01:09:51 +0900

    🎨Design : OR-29

--- theme/theme.ts
+++ theme/theme.ts
@@ -1,11 +1,18 @@
 const colors = {
   GREEN: '#1CC86E',
   GREEN_LIGHT: '#DAFFC9',
-  BLACK: '#000000',
-  GRAY_40: '#646464',
-  GRAY_30: '#9D9D9D',
-  GRAY_20: '#A3A3A3',
-  GRAY_10: '#DFDFDF',
+  BLACK_20: '#000000',
+  BLACK_10: '#111111',
+  GRAY_100: '#646464',
+  GRAY_90: '#787A77',
+  GRAY_80: '#868686',
+  GRAY_70: '#9D9D9D',
+  GRAY_60: '#A3A3A3',
+  GRAY_50: '#C7C7C7',
+  GRAY_40: '#DFDFDF',
+  GRAY_30: '#E4E4E4',
+  GRAY_20: '#F3F3F3',
+  GRAY_10: '#F5F5F5',
   WHITE: '#FFFFFF',
 };
 
