66d9a38e10c7e9630d0ca663ef0cbbcbc9462256
Jiwon <sjiwon9303@gmail.com>
2023-07-24 02:06:40 +0900

    ✨Feat : OR-27

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-const Button = () => {
-  return <button>버튼</button>;
+import { ButtonContainer } from './ButtonStyle';
+export interface ButtonProps {
+  width: number;
+  radius: string;
+  bgColor: string;
+  // onClick?: (name: string) => void;
+}
+
+const Button = (
+  { children }: { children: React.ReactNode },
+  { width, radius, bgColor }: ButtonProps
+) => {
+  return (
+    <ButtonContainer width={width} radius={radius} bgColor={bgColor}>
+      {children}
+    </ButtonContainer>
+  );
+};
+
+Button.deafultProps = {
+  width: 42,
+  radius: '10',
+  bgColor: 'black',
 };
 
 export default Button;
--- components/ButtonStyle.ts
+++ components/ButtonStyle.ts
@@ -1,1 +1,23 @@
 //버튼스타일
+'use client';
+import { styled } from 'styled-components';
+import { ButtonProps } from './Button';
+
+type ButtonStyleProps = ButtonProps;
+
+export const ButtonContainer = styled.button<ButtonProps>`
+  width: ${({ width }) => `${width}px`};
+  background-color: ${({ theme, bgColor }) =>
+    bgColor ? bgColor : theme.colors.BLACK_00};
+  color: white;
+  font-size: 16px;
+  text-align: center;
+  border-radius: ${({ theme, radius }) =>
+    radius === 'small'
+      ? theme.borderRadius.small
+      : radius === 'medium'
+      ? '20px'
+      : '30px'};
+  border: 0;
+  height: ${({ radius }) => (radius === 'small' ? '48px' : '45px')};
+`;
