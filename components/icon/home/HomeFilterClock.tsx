ae83a43fffe26deea376551dc96273ecaeb86c1d
rosenfence <rosenfence@gmail.com>
2023-07-24 23:39:28 +0900

    ✨Feat : OR-28

---
+++ components/icon/home/HomeFilterClock.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeFilterClock = () => {
+  return (
+    <svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M6.625 5.75H4.625C4.418 5.75 4.25 5.582 4.25 5.375V2.375C4.25 2.168 4.418 2 4.625 2C4.832 2 5 2.168 5 2.375V5H6.625C6.832 5 7 5.168 7 5.375C7 5.582 6.832 5.75 6.625 5.75ZM5 0C2.239 0 0 2.239 0 5C0 7.761 2.239 10 5 10C7.761 10 10 7.761 10 5C10 2.239 7.761 0 5 0Z'
+        fill='#99E078'
+      />
+    </svg>
+  );
+};
+
+export default HomeFilterClock;
---
+++ components/icon/home/HomeSearch.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomeSearch = () => {
+  return (
+    <svg width='17' height='16' viewBox='0 0 17 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M11.626 10.8483L15.8436 14.9339'
+        stroke='#868686'
+        strokeWidth='2'
+        strokeLinecap='round'
+      />
+      <circle cx='6.64343' cy='6.64343' r='5.64343' stroke='#868686' strokeWidth='2' />
+    </svg>
+  );
+};
+
+export default HomeSearch;
