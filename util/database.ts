a3b9290b98a8340b6dd5ab3057459180d3f0a114
rosenfence <rosenfence@gmail.com>
2023-07-28 17:06:12 +0900

    ✨Feat : OR-35

---
+++ util/database.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+const url = process.env.NEXT_PUBLIC_MONGODB_URL;
+const options = { useNewUrlParser: true };
+let connectDB: any;
+
+if (process.env.NODE_ENV === 'development') {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url, options).connect();
+}
+export { connectDB };
