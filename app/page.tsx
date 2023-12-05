299e01e5f9d4ba547fb4973adfa29439762ffee3
Jiwon <sjiwon9303@gmail.com>
2023-12-05 22:58:34 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import NaverSearch from '@/components/NaverSearch';
 export default function Home() {
   return (
     <main>
-      <NaverMap lat={37.3595704} lng={127.105399} />
-      <NaverSearch />
+      <div style={{ position: 'relative' }}>
+        <div style={{ position: 'absolute', zIndex: 1 }}>필터</div>
+        <NaverMap lat={37.3595704} lng={127.105399} />
+      </div>
+      {/* <NaverSearch /> */}
     </main>
   );
 }
